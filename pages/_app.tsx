7c953526f664322212fba36c3ba4e2902e4b5605
Elchinbek Jumanazarov <elchinbekjumanazarov@gmail.com>
2024-06-11 12:52:39 +0500

    auth: signup worked

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,37 @@
-import "@/styles/globals.css";
+import '@/styles/globals.css'
+import { PlasmicRootProvider } from "@plasmicapp/react-web";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import {usePlasmicAuthData} from "../utils/usePlasmicAuth"
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+
+
+
+function PlasmicRootProviderWithUser(props: { children: React.ReactNode }) {
+    const {isUserLoading, plasmicUser, plasmicUserToken} = usePlasmicAuthData();
+    return (
+        <PlasmicRootProvider
+            Head={Head}
+            isUserLoading={isUserLoading}
+            user={plasmicUser}
+            userAuthToken={plasmicUserToken}
+        >
+            {props.children}
+        </PlasmicRootProvider>
+    );
+}
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+    if (pageProps.withoutUseAuth) {
+        return (
+            <PlasmicRootProvider Head={Head}>
+                <Component {...pageProps} />
+            </PlasmicRootProvider>
+        );
+    }
+    return (
+        <PlasmicRootProviderWithUser>
+            <Component {...pageProps} />
+        </PlasmicRootProviderWithUser>
+    );
 }
